b51dd7bd592b816a5319cd2d6b61e7a43fb75230
Siyu Yang <siyu.yang@student.ecp.fr>
2014-12-02 20:52:09 +0100

    Some changes to DiskPhysical.java

--- VFS_project/src/Core/DiskPhysical.java
+++ VFS_project/src/Core/DiskPhysical.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.InputStreamReader;
+import java.io.InputStreamReader; 
 import java.nio.LongBuffer;
 
 public class DiskPhysical {
